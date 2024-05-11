bb1627595fc75c113208e0ce43c67363f005ee1e
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 13:28:23 +0200

    Removed usings outside of projects

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,7 +1,7 @@
 global using System.Collections.Generic;
 global using System;
 global using System.Linq;
-global using SunamoExceptions.InSunamoIsDerivedFrom;
+
 global using System.Threading.Tasks;
 global using System.Data;
 global using MixERP.Net.VCards;
