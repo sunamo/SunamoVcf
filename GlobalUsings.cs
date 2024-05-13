b6e0370672057985b9487f7d110c016ece992959
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-13 21:32:26 +0200

    Removed all ns to Sunamo*

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -8,7 +8,7 @@ global using MixERP.Net.VCards;
 global using MixERP.Net.VCards.Models;
 global using MixERP.Net.VCards.Serializer;
 global using MixERP.Net.VCards.Types;
-global using SunamoVcf.Enums;
+
 global using System.Text;
 global using System.Collections.Generic;
 global using System.Collections;
