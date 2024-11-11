07f1373cb482c78a12b07e460e055d7f2dd2c2c8
sunamo.cz <radek.jancik@sunamo.cz>
2024-11-11 23:27:50 +0100

    24.11.11.1 Fix empty string for spaces

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -16,3 +16,5 @@ global using System.Diagnostics;
 global using System.Reflection;
 global using System.Net;
 global using System.Diagnostics.CodeAnalysis;
+global using SunamoVcf;
+global using SunamoVcf._sunamo;
