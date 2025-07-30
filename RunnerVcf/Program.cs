9b35b4309d25c599a40f18c466932adbe12c8c4a
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-30 23:29:30 +0200

    Auto-commit before force push

---
+++ RunnerVcf/Program.cs
@@ -0,0 +1,9 @@
+namespace RunnerVcf;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ SunamoVcf.Tests/VcfHelperTests.cs
@@ -0,0 +1,12 @@
+namespace SunamoVcf.Tests;
+
+public class VcfHelperTests
+{
+    [Fact]
+    public void VcfHelperParseTest()
+    {
+        var l = VcfHelper.Parse(@"D:\_Test\sunamo\SunamoVcf\contacts.vcf");
+
+        int i = 0;
+    }
+}
